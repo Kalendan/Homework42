f5a79c0cb26eeead02f60755243407d293e6c6d8
nadja <nadja.kalenda31@gmail.com>
2024-07-21 16:49:28 +0200

    Initial commit

---
+++ src/FileMain.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Exceptions1 {
+    public static void main(String[] args) {
+        try {
+            File file = new File("Numbers.txt");
+            FileReader fileReader = new FileReader(String.valueOf(file));
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            Integer integer = bufferedReader.readLine();
+            System.out.println(bufferedReader.readLine());
+        } catch (FileNotFoundException e) {
+            System.out.println("no found");
+
+        }
+    }
+}
+
+
+//Создать 2 новых вида исключений
+//Один "проверяемый"
+//Один "непроверяемый"
+
+//Решить две задачи:
+
+//Написать метод. В него вводится целое число - он выводит сумму всех чисел от 1 до этого числа.
+//Если сумма вышла больше 10_000, то бросьте непроверяемое исключение.
+
+//Написать метод. В него вводится 2 числа. Метод ничего не возвращает, но если второе число больше первого,
+//то бросьте проверяемое исключение.
+
+//Если это необходимо - используйте try-catch
+
+//** числа для метода считать из файла. оформит его чтение правильным образом
---
+++ src/UncheckedException.java
@@ -0,0 +1,14 @@
+
+
+
+public class UnheckedException extends Exception {
+
+    public int myUncheckedException(int Number) {
+        int sum = 0;
+        for (int i = 1; i < Number; i++) {
+            sum = +i;
+        }
+        return sum;
+    }
+
+}
